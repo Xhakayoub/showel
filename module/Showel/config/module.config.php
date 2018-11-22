49a145bae0466ff2a409e64b52fa3d5e6d02e07a
mouissi <mouissi.ayoub@gmail.com>
2018-11-22 16:32:06 +0100

    tp module thyp

--- module/Showel/config/module.config.php
+++ module/Showel/config/module.config.php
@@ -1,13 +1,34 @@
 <?php
-return array(
-     'controllers' => array(
-         'invokables' => array(
-             'Showel\Controller\Showel' => 'Showel\Controller\ShowelController',
-         ),
-     ),
-     'view_manager' => array(
-         'template_path_stack' => array(
-             'showel' => __DIR__ . '/../view',
-         ),
-     ),
- );
+ return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Showel\Controller\Showel' => 'Showel\Controller\ShowelController',
+        ),
+    ),
+
+   
+    'router' => array(
+        'routes' => array(
+            'showel' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/showel[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Showel\Controller\Showel',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'showel' => __DIR__ . '/../view',
+        ),
+    ),
+);
