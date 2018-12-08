f98d6c4837e1907ab798e72f02fcb0b20ac02c16
mouissi <mouissi.ayoub@gmail.com>
2018-12-08 22:30:02 +0100

    facedetector

---
+++ src/upload.php
@@ -0,0 +1,29 @@
+<?php
+
+
+include "FaceDetector.php";
+
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+echo "img id='blah' src='http://placehold.it/180' alt='your image' />";
+$file= $_FILES["fileToUpload"]["name"];
+$detector = new FaceDetector('detection.dat');
+$detector->faceDetect($file);
+$detector->toJpeg();
+
+
+?>
